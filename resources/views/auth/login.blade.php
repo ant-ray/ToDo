542ba0571f01b28baf5d7a5b01588b774ff2ce9f
ant-ray <y.y.baske.74cgmail.com>
2022-11-28 15:54:27 +0000

    テスト

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,7 @@
       <div class="col col-md-offset-3 col-md-6">
         <nav class="panel panel-default">
           <div class="panel-heading">ログイン</div>
+          <p>ログインしてください</p>
           <div class="panel-body">
             @if($errors->any())
               <div class="alert alert-danger">
