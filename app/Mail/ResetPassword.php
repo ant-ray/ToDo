db93eea05e68c2b0ec4f6d36303f9e8881867ac2
ant-ray <y.y.baske.74cgmail.com>
2022-11-28 06:42:36 +0000

    first commit

---
+++ app/Mail/ResetPassword.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ResetPassword extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private $token;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param $token
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+
+}
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+use App\Models\Folder;
+use App\Policies\FolderPolicy;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+
+    protected $policies = [
+        Folder::class => FolderPolicy::class,
+    ];
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
